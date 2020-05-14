bb96604d45c98217a6150127179c3407fa978988
sally loaui <42303427+SallyLouai@users.noreply.github.com>
2020-05-15 02:35:12 +0300

    Update Variables_interaction.dart

--- Variables_interaction.dart
+++ Variables_interaction.dart
@@ -7,6 +7,8 @@ void main() {
   String name="sally";
   print(name + secondName);
   print(name+secondName);
+  //we can use this $ dollar sign to place the varible into the sentence 
+  print ("my name is $name ");
 // print(name + secondName+num1);can not be assigned becuase they are diffrent type 
   //in dart the + sign mean to sum . 
   print( num1 +num2 );
