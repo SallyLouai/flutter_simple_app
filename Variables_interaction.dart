0d6e04f985751b30255d4b94c62a76eb4b3e717f
sally loaui <42303427+SallyLouai@users.noreply.github.com>
2020-05-15 02:28:58 +0300

    Create Variables_interaction.dart

---
+++ Variables_interaction.dart
@@ -0,0 +1,13 @@
+void main() {
+  
+  //varible interaction.
+  String secondName="louai";
+  int num1=33;
+  int num2=44;
+  String name="sally";
+  print(name + secondName);
+  print(name+secondName);
+// print(name + secondName+num1);can not be assigned becuase they are diffrent type 
+  //in dart the + sign mean to sum . 
+  print( num1 +num2 );
+}
